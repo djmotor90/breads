edd99e1b44853b8003d2fb0fb60f94fa5938d76d
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-09-13 19:54:26 -0500

    1

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}
+  ).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
+
+
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
